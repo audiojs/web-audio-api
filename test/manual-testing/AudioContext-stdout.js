b5b48836b5d5110ecd1b7f3e1b4f1bb91fc0f114
Sébastien Piquemal <sebpiq@gmail.com>
2015-03-26 21:31:42 +0200

    quick fix to be able to write to stdout

---
+++ test/manual-testing/AudioContext-stdout.js
@@ -0,0 +1,18 @@
+if (require.main === module) { // Just to avoid mocha running this
+  var fs = require('fs')
+    , AudioContext = require('../../build/AudioContext')
+    , context = new AudioContext
+
+  context.outStream = process.stdout
+
+  fs.readFile(__dirname + '/sounds/powerpad.wav', function(err, buffer) {
+    if (err) throw err
+    context.decodeAudioData(buffer, function(audioBuffer) {
+      var bufferNode = context.createBufferSource()
+      bufferNode.connect(context.destination)
+      bufferNode.buffer = audioBuffer
+      bufferNode.loop = true
+      bufferNode.start(0)
+    })
+  })
+}
