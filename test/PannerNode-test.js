924947c41b3459c5b976c369457b959bf56899ad
fand <fand@gmork.in>
2016-02-25 01:38:02 +0900

    Add tests for setPosition, setVelocity

--- test/PannerNode-test.js
+++ test/PannerNode-test.js
@@ -8,10 +8,17 @@ var assert = require('assert')
 
 describe('PannerNode', function() {
 
-  describe('_tick', function() {
+  var helpers = require('./helpers')({ approx : 0.01 })
+    , dummyContext
 
-    var helpers = require('./helpers')({ approx : 0.01 })
-      , dummyContext
+  var testBlockGain = function(block, gainL, gainR) {
+    assert.equal(block.numberOfChannels, 2)
+    assert.equal(block.length, BLOCK_SIZE)
+    helpers.assertAllValuesApprox(block.getChannelData(0), gainL)
+    helpers.assertAllValuesApprox(block.getChannelData(1), gainR)
+  }
+
+  describe('_tick', function() {
 
     beforeEach(function() {
       dummyContext = {
@@ -25,7 +32,6 @@ describe('PannerNode', function() {
     it('should apply the gain for panner position', function() {
       var pannerNode = new PannerNode(dummyContext)
       var sourceNode = new AudioNode(dummyContext, 0, 1)
-      var block
 
       sourceNode.connect(pannerNode)
       sourceNode._tick = function() {
@@ -71,7 +77,7 @@ describe('PannerNode', function() {
         },
       }
 
-      Object.keys(cases).forEach((key) => {
+      Object.keys(cases).forEach(function(key) {
         var c = cases[key]
 
         var pos = c.pos
@@ -81,14 +87,47 @@ describe('PannerNode', function() {
         pannerNode.setPosition(pos[0], pos[1], pos[2])
 
         dummyContext.currentTime++
-        block = pannerNode._tick()
-        assert.equal(block.numberOfChannels, 2)
-        assert.equal(block.length, BLOCK_SIZE)
-        helpers.assertAllValuesApprox(block.getChannelData(0), gain[0], key)
-        helpers.assertAllValuesApprox(block.getChannelData(1), gain[1], key)
+        var block = pannerNode._tick()
+        testBlockGain(block, gain[0], gain[1])
       })
     })
 
+  })
+
+  describe('setPosition', function() {
+    it('throws TypeErrors for invalid parameter', function() {
+      assert.throws(function() {
+        new PannerNode(dummyContext).setPosition(1)
+      }, TypeError)
+      assert.throws(function() {
+        new PannerNode(dummyContext).setPosition(1, 2)
+      }, TypeError)
+      assert.throws(function() {
+        new PannerNode(dummyContext).setPosition(NaN, NaN, NaN)
+      }, TypeError)
+
+      assert.doesNotThrow(function() {
+        new PannerNode(dummyContext).setPosition(1, 2, 3)
+      }, 'Position must be a 3D vector of finite numbers')
+    })
+  })
+
+  describe('setVelocity', function() {
+    it('throws TypeErrors for invalid parameter', function() {
+      assert.throws(function() {
+        new PannerNode(dummyContext).setVelocity(1)
+      }, TypeError)
+      assert.throws(function() {
+        new PannerNode(dummyContext).setVelocity(1, 2)
+      }, TypeError)
+      assert.throws(function() {
+        new PannerNode(dummyContext).setVelocity(NaN, NaN, NaN)
+      }, TypeError)
+
+      assert.doesNotThrow(function() {
+        new PannerNode(dummyContext).setVelocity(1, 2, 3)
+      }, 'Velocity must be a 3D vector of finite numbers')
+    })
   })
 
 })
