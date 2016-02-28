3e95576115cbc5dc949a729e2bbfe770e19818ad
fand <fand@gmork.in>
2016-02-28 23:30:11 +0900

    Add manual-test file for PannerNode

---
+++ test/manual-testing/PannerNode-sound-output.js
@@ -0,0 +1,42 @@
+if (require.main === module) { // Just to avoid mocha running this
+  var fs = require('fs')
+    , AudioContext = require('../../build/AudioContext')
+    , context = new AudioContext
+    , Speaker = require('speaker')
+
+  console.log('encoding format : '
+    + context.format.numberOfChannels + ' channels ; '
+    + context.format.bitDepth + ' bits ; '
+    + context.sampleRate + ' Hz'
+  )
+  context.outStream = new Speaker({
+    channels: context.format.numberOfChannels,
+    bitDepth: context.format.bitDepth,
+    sampleRate: context.sampleRate
+  })
+
+  fs.readFile(__dirname + '/sounds/powerpad.wav', function(err, buffer) {
+    if (err) throw err
+    context.decodeAudioData(buffer, function(audioBuffer) {
+      var bufferNode = context.createBufferSource()
+      var panner     = context.createPanner()
+
+      bufferNode.connect(panner)
+      panner.connect(context.destination)
+
+      var i = 0
+      setInterval(function () {
+        var t = (i / 36) * Math.PI
+        var x = Math.cos(t)
+          , y = 0
+          , z = Math.sin(t)
+        panner.setPosition(x, y, z)
+        i++
+      }, 100)
+
+      bufferNode.buffer = audioBuffer
+      bufferNode.loop = true
+      bufferNode.start(0)
+    })
+  })
+}
