e08704d0fa37be540163b2ea79c9bf9a97647924
fand <fand@gmork.in>
2016-02-28 15:27:43 +0900

    Add tests for Panner / Add comments

---
+++ test/Panner-test.js
@@ -0,0 +1,9 @@
+var assert = require('assert')
+  , Panner = require('../build/PannerNode/Panner')
+
+describe('Panner', function() {
+  it('must not be used directly', function() {
+    const p = new Panner()
+    assert.throws(function() { p.pan() })
+  })
+})
