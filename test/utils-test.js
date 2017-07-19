0e44d23db56d73903bee8efd33ee0c7018c2f721
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2017-07-19 22:05:22 +0900

    Add test cases for `decodeAudioData`

--- test/utils-test.js
+++ test/utils-test.js
@@ -95,7 +95,7 @@ describe('utils', function() {
       helpers.assertAllValuesApprox(blocks[20].getChannelData(1), -1)
     }
 
-    it('should decode a 16b mono wav', function(done) {
+    it('should decode a 16b mono wav (by callback)', function(done) {
       fs.readFile(__dirname + '/sounds/steps-mono-16b-44khz.wav', function(err, buf) {
         if (err) throw err
         utils.decodeAudioData(buf, function(err, audioBuffer) {
@@ -109,7 +109,21 @@ describe('utils', function() {
       })
     })
 
-    it('should decode a 16b stereo wav', function(done) {
+    it('should decode a 16b mono wav (by Promise)', function(done) {
+      fs.readFile(__dirname + '/sounds/steps-mono-16b-44khz.wav', function(err, buf) {
+        if (err) throw err
+        utils.decodeAudioData(buf).then(function(audioBuffer) {
+          assert.equal(audioBuffer.numberOfChannels, 1)
+          assert.equal(audioBuffer.length, 21 * 4410)
+          assert.equal(audioBuffer.sampleRate, 44100)
+          testStepsMono(reblock(audioBuffer, 4410), helpers)
+          done()
+        }).catch(function(err) {
+        })
+      })
+    })
+
+    it('should decode a 16b stereo wav (by callback)', function(done) {
       fs.readFile(__dirname + '/sounds/steps-stereo-16b-44khz.wav', function(err, buf) {
         if (err) throw err
         utils.decodeAudioData(buf, function(err, audioBuffer) {
@@ -123,7 +137,21 @@ describe('utils', function() {
       })
     })
 
-    it('should decode a 16b stereo mp3', function(done) {
+    it('should decode a 16b stereo wav (by Promise)', function(done) {
+      fs.readFile(__dirname + '/sounds/steps-stereo-16b-44khz.wav', function(err, buf) {
+        if (err) throw err
+        utils.decodeAudioData(buf).then(function(audioBuffer) {
+          assert.equal(audioBuffer.numberOfChannels, 2)
+          assert.equal(audioBuffer.length, 21 * 4410)
+          assert.equal(audioBuffer.sampleRate, 44100)
+          testStepsStereo(reblock(audioBuffer, 4410), helpers)
+          done()
+        }).catch(function(err) {
+        })
+      })
+    })
+
+    it('should decode a 16b stereo mp3 (by callback)', function(done) {
       fs.readFile(__dirname + '/sounds/steps-stereo-16b-44khz.mp3', function(err, buf) {
         if (err) throw err
         utils.decodeAudioData(buf, function(err, audioBuffer) {
@@ -151,12 +179,51 @@ describe('utils', function() {
       })
     })
 
-    it('should return an error if the format couldn\'t be recognized', function(done) {
+    it('should decode a 16b stereo mp3 (by Promise)', function(done) {
+      fs.readFile(__dirname + '/sounds/steps-stereo-16b-44khz.mp3', function(err, buf) {
+        if (err) throw err
+        utils.decodeAudioData(buf).then(function(audioBuffer) {
+          var block1 = _.range(4410).map(function() { return 1 })
+            , blockm1 = _.range(4410).map(function() { return -1 })
+
+          assert.equal(audioBuffer.numberOfChannels, 2)
+          assert.ok(audioBuffer.length >= 21 * 4410)
+          assert.equal(audioBuffer.sampleRate, 44100)
+
+          // Strip the silence at the beginning of the mp3 file
+          audioBuffer = audioBuffer.slice(audioBuffer.length - 21 * 4410)
+
+          // Strip the -1 and 1 blocks, cause encoding makes them totally wrong
+          audioBuffer = audioBuffer.slice(4410, audioBuffer.length - 4410)
+
+          // Add fake blocks, just for the tests
+          audioBuffer = AudioBuffer.fromArray([blockm1, block1], 44100).concat(audioBuffer)
+          audioBuffer = audioBuffer.concat(AudioBuffer.fromArray([block1, blockm1], 44100))
+
+          testStepsStereo(reblock(audioBuffer, 4410), require('./helpers')({approx: 0.2}))
+          done()
+        }).catch(function(err) {
+        })
+      })
+    })
+
+    it('should return an error if the format couldn\'t be recognized (by callback)', function(done) {
       fs.readFile(__dirname + '/sounds/generateFile.pd', function(err, buf) {
         if (err) throw err
         utils.decodeAudioData(buf, function(err, audioBuffer) {
           assert.ok(err)
           assert.ok(!audioBuffer)
+          done()
+        })
+      })
+    })
+
+    it('should return an error if the format couldn\'t be recognized (by Promise)', function(done) {
+      fs.readFile(__dirname + '/sounds/generateFile.pd', function(err, buf) {
+        if (err) throw err
+        utils.decodeAudioData(buf).then(function(audioBuffer) {
+        }).catch(function(err) {
+          assert.ok(err)
           done()
         })
       })
