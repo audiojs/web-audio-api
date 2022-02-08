7becbc6979ec9f3b064e2073c9503307502423e8
Dmitry Iv <df.creative@gmail.com>
2022-02-08 14:52:11 -0500

    Update src/AudioContext.js
    Co-authored-by: danigb <danigb@gmail.com>

--- src/AudioContext.js
+++ src/AudioContext.js
@@ -1,6 +1,6 @@
 import events from 'events'
 import * as utils from './utils.js'
-import * as constants from './constants.js'
+import { BLOCK_SIZE } from './constants.js'
 import AudioBuffer from './AudioBuffer.js'
 import AudioListener from './AudioListener.js'
 import AudioDestinationNode from './AudioDestinationNode.js'
